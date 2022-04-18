0e20738aea475204912d0f6c31c07c6c9628aabc
Ramzan <murdagaliev95@gmail.com>
2022-04-18 18:02:36 +0300

    added new function for videoPlayer

--- src/js/main.js
+++ src/js/main.js
@@ -5,47 +5,50 @@ import Difference from './modules/difference'
 import Form from './modules/form'
 
 window.addEventListener('DOMContentLoaded', () => {
-	const slider = new MainSlider({ container: '.page', btns: '.next' })
-	slider.render()
+    const slider = new MainSlider({
+        container: '.page',
+        btns: '.next'
+    })
+    slider.render()
 
-	const modulePageSlider = new MainSlider({
-		container: '.moduleapp',
-		btns: '.next',
-		btnNext: '.nextmodule',
-		btnPrev: '.prevmodule',
-	})
-	modulePageSlider.render()
+    const modulePageSlider = new MainSlider({
+        container: '.moduleapp',
+        btns: '.next',
+        btnNext: '.nextmodule',
+        btnPrev: '.prevmodule',
+    })
+    modulePageSlider.render()
 
-	const showUpSlider = new MiniSlider({
-		container: '.showup__content-slider',
-		prev: '.showup__prev',
-		next: '.showup__next',
-		activeClass: 'card-active',
-		animate: true,
-	})
-	showUpSlider.init()
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        prev: '.showup__prev',
+        next: '.showup__next',
+        activeClass: 'card-active',
+        animate: true,
+    })
+    showUpSlider.init()
 
-	const modulesSlider = new MiniSlider({
-		container: '.modules__content-slider',
-		prev: '.modules__info-btns .slick-prev',
-		next: '.modules__info-btns .slick-next',
-		activeClass: 'card-active',
-		animate: true,
-		autoplay: true,
-	})
-	modulesSlider.init()
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        prev: '.modules__info-btns .slick-prev',
+        next: '.modules__info-btns .slick-next',
+        activeClass: 'card-active',
+        animate: true,
+        autoplay: true,
+    })
+    modulesSlider.init()
 
-	const feedSlider = new MiniSlider({
-		container: '.feed__slider',
-		prev: '.feed__slider .slick-prev',
-		next: '.feed__slider .slick-next',
-		activeClass: 'feed__item-active',
-	})
-	feedSlider.init()
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        prev: '.feed__slider .slick-prev',
+        next: '.feed__slider .slick-next',
+        activeClass: 'feed__item-active',
+    })
+    feedSlider.init()
 
-	const player = new VideoPlayer('.showup .play', '.overlay')
-	player.init()
+    new VideoPlayer('.showup .play', '.overlay').init()
+    new VideoPlayer('.module__video-item .play', '.overlay').init()
 
-	new Difference('.officerold', '.officernew', '.officer__card-item').init()
-	new Form('.form').init()
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
+    new Form('.form').init()
 })
--- src/js/modules/difference.js
+++ src/js/modules/difference.js
@@ -1,37 +1,42 @@
 export default class Difference {
-	constructor(oldOficer, newOficer, items) {
-		this.oldOficer = document.querySelector(oldOficer)
-		this.newOficer = document.querySelector(newOficer)
-		this.oldItems = this.oldOficer.querySelectorAll(items)
-		this.newItems = this.newOficer.querySelectorAll(items)
-		this.oldCounter = 0
-		this.newCounter = 0
-	}
+    constructor(oldOfficer, newOfficer, items) {
+        try {
+            this.oldOfficer = document.querySelector(oldOfficer);
+            this.newOfficer = document.querySelector(newOfficer);
+            this.oldItems = this.oldOfficer.querySelectorAll(items);
+            this.newItems = this.newOfficer.querySelectorAll(items);
+            this.oldCounter = 0;
+            this.newCounter = 0;
+        } catch (e) {}
+    }
 
-	bindTriggers(plus, counter, items) {
-		plus.querySelector('.plus').addEventListener('click', () => {
-			if (counter !== items.length - 2) {
-				items[counter].style.display = 'flex'
-				counter++
-			} else {
-				items[counter].style.display = 'flex'
-				items[items.length - 1].remove()
-			}
-		})
-	}
+    bindTriggers(container, items, counter) {
+        container.querySelector('.plus').addEventListener('click', () => {
+            if (counter !== items.length - 2) {
+                items[counter].style.display = 'flex';
+                counter++;
+            } else {
+                items[counter].style.display = 'flex';
+                items[items.length - 1].remove();
+            }
+        });
+    }
 
-	hideItems(block) {
-		block.forEach((item, i, arr) => {
-			if (i !== arr.length - 1) {
-				item.style.display = 'none'
-			}
-		})
-	}
+    hideItems(items) {
+        items.forEach((item, i, arr) => {
+            if (i !== arr.length - 1) {
+                item.style.display = 'none';
+            }
+        });
+    }
 
-	init() {
-		this.hideItems(this.oldItems)
-		this.hideItems(this.newItems)
-		this.bindTriggers(this.oldOficer, this.oldCounter, this.oldItems)
-		this.bindTriggers(this.newOficer, this.newCounter, this.newItems)
-	}
+    init() {
+        try {
+            this.hideItems(this.oldItems);
+            this.hideItems(this.newItems);
+
+            this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+            this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
+        } catch (e) {}
+    }
 }
